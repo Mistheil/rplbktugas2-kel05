2183d3f98d3804a4a62919f7afb3b7ef88638ef0
GUNAWAN PRASETYA <gunawan29092000@gmail.com>
2022-10-26 18:44:03 +0700

    Comit App.js Gunawan Prasetya

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
+
 import React from "react";
 import "./App.css";
 import Home from "./Order/Home";
 
 class App extends React.Component {
+  state = {
+    komponen1 : false,
+  };
+
+  ubahkomponen1 = () => {
+    this.setState((state) => {
+      return { komponen1: !this.state.komponen1};
+    });
+  };
   
   render() {
     return (
-      <div>
- 
+      <div className="App">
+        <div>
+          <h1>Burjo Jaya Kusuma Winarno</h1>
+          <h4>[Kelompok 5]</h4>
+
+          <button className="Button" onClick={this.ubahkomponen1}>
+            {this.state.komponen1 ? "Sembunyikan Menu" : "Lihat Menu"}
+          </button>
+          <div style={{ border: "4px solid black" }}>
+            {this.state.komponen1 ? <Home /> : ""}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+
